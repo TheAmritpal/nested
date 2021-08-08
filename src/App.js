29433495af7af11e0ff63067a06db5a3d9c65de1
TheAmritpal <45561952+TheAmritpal@users.noreply.github.com>
2021-08-08 18:12:36 +0530

    nested routes in react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,163 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams,
+} from "react-router-dom";
+
+const topics = [
+  {
+    name: "React Router",
+    id: "react-router",
+    description: "Declarative, component based routing for React",
+    resources: [
+      {
+        name: "URL Parameters",
+        id: "url-parameters",
+        description: "url Parameters Desciptoin",
+        url: "https://react.com",
+      },
+      {
+        name: "Programaticslly Navigate",
+        id: "programmiticaly-navigate",
+        description: "url Parameters Desciptoin",
+        url: "https://react.com",
+      },
+    ],
+  },
+  {
+    name: "React Js",
+    id: "react-js",
+    description: "A Javascript Library for building user interface",
+    resources: [
+      {
+        name: "React Lifecycle Events",
+        id: "react-lifecycle-events",
+        description:
+          "React lifecycle allows you to ahfhakdh akjshdakjdjkd lhaslkdjkaljd lkajsldkjaj dlaj d",
+        url: "https://react.com",
+      },
+      {
+        name: "React AHA Moments",
+        id: "react-aha-moments",
+        description:
+          "React aha moments to ahfhakdh akjshdakjdjkd lhaslkdjkaljd lkajsldkjaj dlaj d",
+        url: "https://react.com",
+      },
+    ],
+  },
+  {
+    name: "Functiional Programming",
+    id: "functional-programming",
+    description:
+      "In computer sience, functioncal programing is akjshdakjdjkd lhaslkdjkaljd lkajsldkjaj dlaj d",
+    resources: [
+      {
+        name: "Imerative vs Decorative Programming",
+        id: "imerative-decorative",
+        description:
+          "A guide to understanding the diffrence between imertative and decorative",
+        url: "https://react.com",
+      },
+      {
+        name: "Building Userinterface with pure functions and functiions composition",
+        id: "fn-composition",
+        description: "A guid to building Ui with pure funvyion  and function composotion",
+        url: "https://react.com",
+      },
+    ],
+  },
+];
+
+function Home() {
+  return <h1>Home</h1>;
+}
+
+function Resource() {
+  const { topicId, subId } = useParams();
+  const topic = topics
+    .find(({ id }) => id === topicId)
+    .resources.find(({ id }) => id === subId);
+
+  return (
+    <div>
+      <h3>{topic.name}</h3>
+      <p>{topic.description}</p>
+      <a href={topic.url}>More info</a>
+    </div>
+  );
+}
+
+function Topic() {
+  const { topicId } = useParams();
+  const { url, path } = useRouteMatch();
+
+  const topic = topics.find(({ id }) => id === topicId);
+
+  return (
+    <div>
+      <h3>{topic.name}</h3>
+      <p>{topic.description}</p>
+      <ul>
+        {topic.resources.map((sub) => (
+          <li key={sub.id}>
+            <Link to={`${url}/${sub.id}`}>{sub.name}</Link>
+          </li>
+        ))}
+      </ul>
+
+      <Route path={`${path}/:subId`}>
+        <Resource />
+      </Route>
+    </div>
+  );
+}
+
+function Topics() {
+  const { path, url } = useRouteMatch();
+
+  return (
+    <div>
+      <h1>Topics</h1>
+      <ul>
+        {topics.map(({ name, id }) => (
+          <li key={id}>
+            <Link to={`${url}/${id}`}>{name}</Link>
+          </li>
+        ))}
+      </ul>
+      <hr />
+      <Route path={`${path}/:topicId`}>
+        <Topic />
+      </Route>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <h1>Home</h1>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+        </ul>
+        <hr />
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/topics">
+          <Topics />
+        </Route>
+      </div>
+    </Router>
   );
 }
 
